a6258baa887aca42b9d345e34150c03fa7f9632a
Madi Nuralin <nmdbzk@gmail.com>
2023-02-21 19:30:02 +0600

    Task complete

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,10 @@
+
+ 
+<html>
+    <head>
+        <title>{{ $title ?? 'Andersen tech task' }}</title>
+    </head>
+    <body>
+         @yield('content')
+    </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\FormController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::post('store', [FormController::class, 'store']);
+Route::get('show/{id}', [FormController::class, 'show']);
